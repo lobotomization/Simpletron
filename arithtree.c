8ff96681c593ccd60c606e44a29244fa06bcd7d5
root <root@SCOOBY.localdomain>
2016-12-12 18:53:43 -0500

    Working virtual machine, basic working arithmetic parser, non-existent compiler

---
+++ arithtree.c
@@ -0,0 +1,303 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+//A stack of strings is used to help parse the arithmetic.
+//Top stores the number of items on the stack
+typedef struct stringstack {
+	unsigned int top;
+    char **stack;
+} Stk;
+//Give pointers to Stk structs their own type
+typedef Stk *StkPtr;
+//Associativity of functions is given its own type
+typedef enum assoc{left, right} assoc_t;
+/*
+* newStk - Returns a pointer to a new stack object
+* pop - Returns the top-most string from the stack after removing it
+* peek - Returns the top-most string from the stack without removing it
+* compute - Applies the arithmetic operator specified by the char c
+* power - Performs exponentiation of long ints
+* parseRPN - This parses and evaluates a stack of arithmetic given in reverse polish notation
+* longToString - Converts a long to a string for placement on a stack
+* printStack - This prints the strings that currently reside on a stack object
+* shunt -
+*   This function uses the shunting yard algorithm.
+*   This is where standard input is converted
+*   into reverse polish notation.
+*   It currently handles addition, multiplication
+*   subtraction, division and exponentiation of
+*   unsigned long ints.
+* precedence - This returns the precedence of an arithmetic operator
+* assoc - This returns the associativity of an operator (left or right)
+* readToken - This parses the user input into numbers and operators
+*/
+StkPtr newStk();
+int push(StkPtr st, char *in);
+char *pop(StkPtr st);
+char *peek(StkPtr st);
+long compute(long a, long b, char c);
+long power(long a, long b);
+long *parseRPN(StkPtr st);
+char *longToString(long l);
+void printStack(StkPtr st);
+StkPtr shunt(char *math);
+int precedence(char *token);
+assoc_t assoc(char *token);
+char *readToken(char **math);
+int main()
+{
+    /*
+	StkPtr test = newStk();
+    printf("sizeof(test->stack) = %lu\n", sizeof(test->stack));
+
+	push(test, "ayylmao\n");
+	push(test, "lmao\n");
+	printf("Test Stack:\n");
+	printStack(test);
+    printf("sizeof(char*) = %lu\n", sizeof(char*));
+    printf("sizeof(test->stack) = %lu\n", sizeof(test->stack));
+	printf("Am I an alien?\n%s", pop(test));
+	char *math = "(123+456)*(789-42)/98";
+	printf("Math: %s\n", math);
+    StkPtr out = shunt(math);
+    printf("Output stack:\n");
+    printStack(out);
+    printf("This expression equals %lu\n", parseRPN(out));*/
+
+    char input[1024];
+    StkPtr arithStack;
+    long *sol;
+    printf("Interactive Math Solver\n");
+    do{
+        printf("M> ");
+        scanf("%s", input);
+        if(strcmp(input, "quit") == 0)
+            break;
+        arithStack = shunt(input);
+        if(arithStack)
+            sol = parseRPN(arithStack);
+        if(sol)
+            printf("%ld\n", *sol);
+    }while(1);
+	return 0;
+}
+
+long *parseRPN(StkPtr st){
+    StkPtr tmp = newStk();
+    long a, b, c, *out;
+    char *a_st, *b_st;
+    for(int i = 0; i < st->top; i++){
+        if(isdigit(**(st->stack + i))){
+            push(tmp, *(st->stack + i));
+        }
+        else{
+            b_st = pop(tmp);   //Pop the strings backwards since this
+            a_st = pop(tmp);  //is how they're stored on the stack
+            if(a_st == NULL || b_st == NULL){
+                printf("Inappropriate number of parameters!\n");
+                return NULL;
+            }
+            b = strtol(b_st, NULL, 10);
+            a = strtol(a_st, NULL, 10);
+            c = compute(a, b, **(st->stack + i));
+            push(tmp, longToString(c));
+        }
+    }
+
+    out = malloc(sizeof(long));
+    *out = strtol(pop(tmp), NULL, 10);
+
+    return out;
+
+}
+
+char *longToString(long l){
+    const int n = snprintf(NULL, 0, "%ld", l);
+    char *buf = (char *)malloc(n+1);
+    int c = snprintf(buf, n+1, "%ld", l);
+    return buf;
+}
+
+long compute(long a, long b, char c){
+    switch(c){
+    case '+':
+        return a+b;
+    case '-':
+        return a-b;
+    case '*':
+        return a*b;
+    case '/':
+        return a/b;
+    case '^':
+        return power(a,b);
+    default:
+        printf("Bad symbol %c detected during parse!\n", c);
+        return 0;
+    }
+}
+
+long power(long a, long b){
+    long out = a;
+    b--;
+    while(b){
+        out *= a;
+        b--;
+    }
+    return out;
+}
+
+StkPtr newStk(){
+	StkPtr out = (StkPtr)calloc(1, sizeof(Stk));
+	out->top = 0;
+	return out;
+}
+
+int push(StkPtr st, char *in){
+	st->stack = (char **)realloc(st->stack, (st->top+1)*sizeof(char*));
+	*(st->stack + st->top) = in;
+	st->top++;
+	return st->top;
+}
+
+char *pop(StkPtr st){
+	if(st == NULL || st->top == 0)
+	return NULL;
+	st->top--;
+	char *out = (char *)malloc(strlen(*(st->stack + st->top)));
+	strcpy(out, *(st->stack + st->top));
+	st->stack = (char **)realloc(st->stack, st->top*sizeof(char*));
+	return out;
+}
+
+char *peek(StkPtr st){
+	if(st == NULL || st->top == 0)
+	return NULL;
+	char *out = (char *)malloc(strlen(*(st->stack + st->top - 1)));
+        strcpy(out, *(st->stack + st->top - 1));
+        return out;
+}
+
+void printStack(StkPtr st){
+    if(st == NULL){
+        printf("(null)\n");
+    }
+    else{
+        for(int i = 0; i < st->top; i++){
+            printf("Stack[%d] = %s\n", i, *(st->stack + i));
+        }
+    }
+}
+
+
+StkPtr shunt(char *math){
+	StkPtr output = newStk(), operators = newStk();
+	char *cursor = math, *tmp, *token;
+	while(token = readToken(&cursor)){
+
+		if(isdigit(*token)){
+			push(output, token);
+            continue;
+		}
+
+        if(*token == '('){
+			push(operators, token);
+			continue;
+		}
+
+        if(*token == ')'){
+            while(tmp = pop(operators)){
+                if(*tmp == '(')
+                    break;
+                push(output, tmp);
+            }
+            if(tmp == NULL){
+                printf("Close bracket without open bracket!\n");
+                return NULL;
+            }
+            continue;
+        }
+
+        /*
+        *   The following if statement should
+        *   only be running if *token != ')'
+        *   and *token != '('
+        *   This is only being implicitly enforced
+        *   due to the above if statements and their
+        *   continue clauses.
+        */
+        //  Precedence of 0 means a bad token was given.
+		if(!isdigit(*token) && precedence(token)){
+
+            if(assoc(token) == left)
+			while(peek(operators) != NULL && precedence(token) <= precedence(peek(operators))){
+				push(output, pop(operators));
+			}
+
+            if(assoc(token) == right)
+			while(peek(operators) != NULL && precedence(token) < precedence(peek(operators))){
+				push(output, pop(operators));
+			}
+
+            push(operators, token);
+            continue;
+		}
+
+
+
+		else
+		{
+			printf("Bad input!\n");
+			printf("Token not recognized: %s\n", token);
+			return NULL;
+		}
+	}
+
+	while(tmp = pop(operators)){
+		if(*tmp == '(' || *tmp == ')'){
+			printf("Mismatched parentheses!\n");
+			return NULL;
+		}
+		push(output, tmp);
+	}
+	return output;
+
+}
+int precedence(char *token){
+	switch(*token){
+		case '+': case '-':
+		return 1;
+		case '*': case '/':
+		return 2;
+		case '^':
+		return 3;
+		default:
+        return 0;
+	}
+}
+
+assoc_t assoc(char *token){
+	if(*token == '^')
+	return right;
+	return left;
+}
+char *readToken(char **math){
+	if(math == NULL || **math == '\0')
+	return NULL;
+	char *cursor = (char *)malloc(strlen(*math));
+	char *base = cursor;
+	if(!isdigit(**math)){
+		*cursor = **math;
+		cursor++;
+		(*math)++;
+		*cursor = '\0';
+		return base;
+	}
+	while(isdigit(**math)){
+		*cursor = **math;
+		cursor++;
+		(*math)++;
+	}
+	*cursor = '\0';
+	return base;
+}
---
+++ simpletron.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#define READ 10 //Takes user input, stores in ##
+#define WRITE 11 //Writes value in ## to console
+#define LOAD 20 //Loads the value in ## into the accumulator
+#define STORE 21 //Stores the accumulator value in ##
+#define ADD 30 //Adds the value in ## to the accumulator
+#define SUBTRACT 31 //Subtracts the value in ## from the accumulator
+#define DIVIDE 32 //Divides the accumulator by the value in ##
+#define MULTIPLY 33 //Multiplys the accumulator by the value in ##
+#define BRANCH 40 //Branches to location ##
+#define BRANCHNEG 41 //Branches to location ## if accumulator < 0
+#define BRANCHZERO 42 //Branches to location ## if accumulator = 0
+#define HALT 43 //Ends program execution
+void interpreter(int *memory);
+void dump(int *memory, int accumulator, int currentIndex);
+main()
+{	//{branch to start 00, counter 01, max of counter 02, current max number 03, input 04, counter increment 05,
+											              //18                          //24                          //30
+//	int memory[100] = {4007,0,7,0,0,1,4300,1002,2002,4206,1004,2004,2103,2001,3005,2101,3102,4230,1004,2004,3103,4124,2004,2103,2001,3005,2101,3102,4230,4018,1103,4300};
+//	int memory[100] = {1009, 1010, 2009, 3110, 4107, 1109, 4300, 1110, 4300, 0000, 0000};
+	int memory[100] = {0};
+	printf("***          Welcome to Simpletron!               ***\n");
+	printf("***   Please enter your program one instruction   ***\n"); 
+	printf("***   (or data word) at a time. I will type the   ***\n");
+        printf("***   location number and a colon. You then type  ***\n");
+        printf("*** the word for that location. Type the sentinel ***\n");
+        printf("***     -99999 to stop entering your program      ***\n");	
+	int instruction, memLocation = 0;
+	do
+	{
+		printf("%02d: ",memLocation);
+		scanf("%d",&instruction);
+		if(instruction == -99999)
+		break;
+		if(instruction < -9999||instruction > 9999)
+		continue;	
+		memory[memLocation] = instruction;
+		memLocation++;	
+	}
+	while(memLocation < 100);
+	printf("*** Program loading completed ***\n");
+	printf("*** Program execution begins  ***\n");
+	interpreter(memory);
+	return 0;
+}
+void interpreter(int *memory)
+{
+	static int accumulator,currentIndex = 0, fetchedAddress,fetchedInstruction;
+	do
+	{
+		fetchedAddress = memory[currentIndex]%100;
+		fetchedInstruction = memory[currentIndex]/100;
+		switch(fetchedInstruction)
+		{
+			case READ:
+			printf("Please enter a number into memory location %02d: ",fetchedAddress);
+			scanf("%d",&memory[fetchedAddress]);
+			currentIndex++;
+			break;
+			case WRITE:
+			printf("The number stored in memory location %02d is: %d\n", fetchedAddress, memory[fetchedAddress]);
+			currentIndex++;
+			break;
+			case LOAD:
+			printf("Memory location %02d, containing the number %d, has been stored in the accumulator\n",fetchedAddress,memory[fetchedAddress]);
+			accumulator = memory[fetchedAddress];
+			currentIndex++;
+			break;
+			case STORE:
+			printf("Memory location %02d now contains %d, the value of the accumulator\n",fetchedAddress,accumulator);
+			memory[fetchedAddress] = accumulator;
+			currentIndex++;
+			break;
+			case ADD:
+			printf("Memory location %02d, containing the number %d, has been added to the accumulator\n",fetchedAddress,memory[fetchedAddress]);
+			accumulator += memory[fetchedAddress];
+			if(accumulator>9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has overflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			if(accumulator<-9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has underflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			printf("Current value of the accumulator: %d\n",accumulator);
+			currentIndex++;
+			break;
+			case SUBTRACT:
+			printf("Memory location %02d, containing the number %d, has been subtracted from the accumulator\n",fetchedAddress,memory[fetchedAddress]);
+			accumulator -= memory[fetchedAddress];
+			if(accumulator>9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has overflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			if(accumulator<-9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has underflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			printf("Current value of the accumulator: %d\n",accumulator);
+			currentIndex++;
+			break;
+			case DIVIDE:
+			printf("Memory location %02d, containing the number %d, has been divided into the accumulator\n",fetchedAddress,memory[fetchedAddress]);
+			if(memory[fetchedAddress]==0)
+			{
+				printf("Divide by zero error issued by memory index %02d, program halted.", currentIndex);
+			}
+			accumulator /= memory[fetchedAddress];
+			if(accumulator>9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has overflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			if(accumulator<-9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has underflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			printf("Current value of the accumulator: %d\n",accumulator);
+			currentIndex++;
+			break;
+			case MULTIPLY:
+			printf("Memory location %02d, containing the number %d, has been multiplied into the accumulator\n",fetchedAddress,memory[fetchedAddress]);
+			accumulator *= memory[fetchedAddress];
+			if(accumulator>9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has overflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			if(accumulator<-9999)
+			{
+				printf("Error at memory index %02d\n",currentIndex);
+				printf("The accumulator has underflowed, program halted.\n");
+				fetchedInstruction = HALT;
+				break;
+			}
+			printf("Current value of the accumulator: %d\n",accumulator);
+			currentIndex++;
+			break;
+			case BRANCH:
+			printf("Branching to index %02d\n",fetchedAddress);
+			currentIndex = fetchedAddress;
+			break;
+			case BRANCHNEG:
+			printf("Branching to index %02d if the accumulator is negative\n", fetchedAddress);
+			printf("Current value of the accumulator: %d\n",accumulator);
+			if(accumulator<0)
+			currentIndex = fetchedAddress;
+			else
+			currentIndex++;
+			break;
+			case BRANCHZERO:
+			printf("Branching to index %02d if the accumulator is zero\n", fetchedAddress);
+			printf("Current value of the accumulator: %d\n", accumulator);
+			if(accumulator==0)
+			currentIndex = fetchedAddress;
+			else
+			currentIndex++;
+			break;
+			case HALT:
+			break;
+			default:
+			printf("Errorneous instruction %02d issued from memory index %02d, program halted.\n", fetchedInstruction, currentIndex);
+			fetchedInstruction = HALT;
+			break;
+		}		
+	}
+	while(fetchedInstruction != HALT);
+	dump(memory, accumulator, currentIndex);
+}
+
+void dump(int *memory, int accumulator, int currentIndex)
+{
+	int units=0,tens=0;
+	printf("REGISTERS\n");
+	printf("Accumulator\t\t%+05d\n", accumulator);
+	printf("Memory Index\t\t   %02d\n", currentIndex);
+	printf("Memory Value\t\t%+05d\n", memory[currentIndex]);
+	printf("MEMORY\n");
+	printf("     ");
+	for(units = 0; units<10; units++)
+	{
+		printf("%4d   ",units);
+	}
+	puts("");
+	for(tens = 0; tens<10; tens++)
+	{
+		printf("%2d  ", 10*tens);
+		for(units = 0; units<10; units++)
+		{
+			printf("%+05d  ",memory[units + 10*tens]);
+		}
+		puts("");
+	}
+}
