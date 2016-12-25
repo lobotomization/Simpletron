6f3e3836624d8d5a1e2cf0a0a982e4752d7c82be
root <root@SCOOBY.localdomain>
2016-12-25 05:03:50 -0500

    Created a prime finding program which properly compiles and works!

--- arithtree.c
+++ arithtree.c
@@ -41,23 +41,55 @@ int main()
 {
 
     //char input[1024] = " 10 rem this is a comment\n 20 input x\n 30 print x\n 40 let y = x*x\n 50 let x = y \n 55 print y\n 60 if y < 10 goto 40\n 70 goto 80\n 80 end";
-    //char input[1024] = " 10 rem This calculates x mod y\n 20 input x\n 30 input y\n 40 let z = x/y\n 50 let r = (x-z*y) \n 55 print r\n 60 end";
+
+    //char input[1024] =\
+     "  10 rem This calculates x mod y \n\
+        20 input x \n\
+        30 input y \n\
+        40 let z = x/y \n\
+        50 let r = (x-z*y) \n\
+        55 print r \n\
+        60 end";
+    //char input[1024] =\
+    " 10 rem This calculates the divisors of x \n\
+      20 input x \n\
+      30 let y = 1 \n\
+      40 let z = x/y \n\
+      50 let r = (x-z*y) \n\
+      60 if r > 0 goto 80 \n\
+      70 if r == 0 goto 110 \n\
+      80 let y = y+1 \n\
+      90 if y > x goto 150 \n\
+      100 goto 40 \n\
+      110 print y \n\
+      120 let y = y+1 \n\
+      130 if y > x goto 150 \n\
+      140 goto 40 \n\
+      150 end ";
+
+      //p is the potential prime, d is the number of divisors, y is a test divisor, q is the integer quotient of p and y
     char input[1024] =\
-    " 10 rem This calculates the divisors of x\n\
-    20 input x \n\
-    30 let y = 1 \n\
-    40 let z = x/y \n\
-    50 let r = (x-z*y) \n\
-    60 if r > 0 goto 80 \n\
-    70 if r == 0 goto 110 \n\
-    80 let y = y+1 \n\
-    90 if y > x goto 150 \n\
-    100 goto 40 \n\
-    110 print y \n\
-    120 let y = y+1 \n\
-    130 if y > x goto 150 \n\
-    140 goto 40 \n\
-    150 end ";
+    " 10 rem This calculates the primes up to 75 \n\
+      20 let p = 0 \n\
+      25 let d = 0 \n\
+      26 let p = p+1 \n\
+      30 let y = 1 \n\
+      40 let q = p/y \n\
+      50 let r = (p-q*y) \n\
+      60 if r > 0 goto 80 \n\
+      70 if r == 0 goto 110 \n\
+      80 let y = y+1 \n\
+      90 if y > p goto 150 \n\
+      100 goto 40 \n\
+      110 let d = d+1 \n\
+      120 let y = y+1 \n\
+      130 if y > p goto 150 \n\
+      140 goto 40 \n\
+      150 if d > 2 goto 170 \n\
+      155 if d < 2 goto 170 \n\
+      160 print p \n\
+      170 if p < 75 goto 25 \n\
+      180 end ";
 
     /*
     StkPtr arithStack;
